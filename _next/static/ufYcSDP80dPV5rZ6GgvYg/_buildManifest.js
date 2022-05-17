7f2dbf3d51855181e8d887be0534f4b20c564f6f
derekpodi <derekpodi@users.noreply.github.com>
2022-05-17 19:03:32 +0000

    deploy: ec9deb5aef2a4b9436387d57374ca26ed3425f34

---
+++ _next/static/ufYcSDP80dPV5rZ6GgvYg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-19ea76a8789e4a74.js"],"/_error":["static/chunks/pages/_error-635a6533d6ac3ca1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
